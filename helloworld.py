0a45ddbbb5acb79e4a313113bb2cc1a0233cce93
Habeeb Surooprajally <habeeb@Habeebs-Mac-mini.local>
2019-12-06 00:48:58 +0000

    and here we go boys we did it

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,17 @@
 #this is for advent of code 2019
 
+import math
+
 print("hello world")
 print("welcome to my program")
+
+F = open("workfile.txt","r")
+lines = F.readlines()
+total = 0
+for x in lines:
+    division = int(x) // 3
+    subtraction = division - 2
+    total = total + subtraction
+
+print "and the answer is"
+print total
